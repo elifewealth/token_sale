355cb2658f425e7004d30dc935ee34331fab40ed
Aaron <aaron@elw.com>
2018-12-05 16:02:18 +0800

    3.5

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -2,9 +2,9 @@ pragma solidity ^0.4.2;
 
 contract DappToken {
 	// Name
-	string public name = "DApp Token";
-	string public symbol = "DAPP";
-	string public standard = "DApp Token v1.0";
+	string  public name = "DApp Token";
+	string  public symbol = "DAPP";
+	string  public standard = "DApp Token v1.0";
 	uint256 public totalSupply;
 
 	event Transfer(
