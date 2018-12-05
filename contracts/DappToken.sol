0b66f836d259f4d8ead95e8adb16bbe8c35ae69f
Aaron <aaron@elw.com>
2018-12-05 13:58:26 +0800

    3. Delegated Transfer

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -12,9 +12,14 @@ contract DappToken {
 		address indexed _to,
 		uint256 _value);
 
-	// transfer
+	// approve
+	event Approval(
+		address indexed _owner,
+		address indexed _spender,
+		uint256 _value);
 
 	mapping(address => uint256) public balanceOf;
+	mapping(address => mapping(address => uint256)) public allowance;
 	// allowance
 
 	function DappToken (uint256 _initialSupply) public {
@@ -34,11 +39,25 @@ contract DappToken {
 		return true;
 	}
 
-	// approve
 	function approve(address _spender, uint256 _value) public returns (bool success) {
-		
+		allowance[msg.sender][_spender] = _value;
+
+		Approval(msg.sender, _spender, _value);
+
 		return true;
 	}
 
-	// transferFrom
+	function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+		require(_value <= balanceOf[_from]);
+		require(_value <= allowance[_from][msg.sender]);
+
+		balanceOf[_from] -= _value;
+		balanceOf[_to] += _value;
+		
+		allowance[_from][msg.sender] -= _value;
+
+		Transfer(_from, _to, _value);
+
+		return true;
+	}
 }
